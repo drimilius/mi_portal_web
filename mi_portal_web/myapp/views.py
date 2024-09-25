a4dd29b46c2bca46e66f06bb4bd491a448c3486c
ToIvanP <drimilius@gmail.com>
2024-09-25 13:27:31 -0300

    mod

--- mi_portal_web/myapp/views.py
+++ mi_portal_web/myapp/views.py
@@ -2,15 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    #return HttpResponse("Bienvenido a tu pagina")
-    return render(request,'myapp/home.html')
-
+   # return HttpResponse("Bienvenido a tu pagina")  # retorna en la pagina home
+    return render(request, 'myapp/home.html')
 def about(request):
-    # return HttpResponse("Acerca de esta pagina")
-    return render(request,'myapp/about.html')
-
+    #return HttpResponse("Acerca de esta pagina")# retorna en una pagina acerca de 
+    return render(request, 'myapp/about.html')
 def contact(request):
-    # return HttpResponse("Pagina de Contacto")
-    return render(request,'myapp/contact.html')
+    #return HttpResponse("Pagina de Contacto") # retorna en pagina de contacto
+    return render(request, 'myapp/contact.html')
+def services(request):
+    #return HttpResponse("Pagina de Servicios") # retorna en servicios
+    return render(request, 'myapp/services.html')
+def blog(request):
+    #return HttpResponse("Pagina del Blog") # retorna en una blog
+    return render(request, 'myapp/blog.html')
 
-# Create your views here.
